af90ba97dff2efdae5e56de86abf0dce88be98f2
gui2one <guillaume.rouault.fx@gmail.com>
2025-02-19 03:31:55 +0100

    install service button is working !!

--- src/control_window.py
+++ src/control_window.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
-
-
+from subprocess import Popen, STDOUT, PIPE
+import sys, os
 class ControlWindow(ctk.CTk):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, **kwargs)
@@ -8,7 +8,22 @@ class ControlWindow(ctk.CTk):
         self.title("Control Window")
         self.geometry("300x200")
         self.resizable(False, False)
+        
+        self.btn_install = ctk.CTkButton(self, text="Install Service", command=self.install_service)
+        self.btn_install.pack(pady=20)
 
+    def install_service(self):
+        if getattr(sys, 'frozen', False):
+            BASE_DIR = os.path.dirname(sys.executable)
+            full_path = f"{BASE_DIR}\\service.exe".replace("\\", "/")
+            cmd = [ f"{full_path}", "install"]
+        else:
+            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+            cmd = ["python", f"{BASE_DIR}/service.py", "install"]
+
+        print(cmd)
+        
+        Popen(cmd)
 
 if __name__ == "__main__":
     window = ControlWindow()
