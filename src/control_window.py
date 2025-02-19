9230a4a62fb5f06aa9af948788a23340ca979096
gui2one <guillaume.rouault.fx@gmail.com>
2025-02-19 03:38:30 +0100

    start, stopand remove service

--- src/control_window.py
+++ src/control_window.py
@@ -7,11 +7,20 @@ class ControlWindow(ctk.CTk):
 
         self.title("Control Window")
         self.geometry("300x200")
-        self.resizable(False, False)
+        # self.resizable(False, False)
         
         self.btn_install = ctk.CTkButton(self, text="Install Service", command=self.install_service)
         self.btn_install.pack(pady=20)
+        
+        self.btn_remove = ctk.CTkButton(self, text="Uninstall Service", command=self.remove_service)
+        self.btn_remove.pack(pady=20)
+        
+        self.btn_start = ctk.CTkButton(self, text="Start Service", command=self.start_service)
+        self.btn_start.pack(pady=20)
 
+        self.btn_stop = ctk.CTkButton(self, text="Stop Service", command=self.stop_service)
+        self.btn_stop.pack(pady=20)
+        
     def install_service(self):
         if getattr(sys, 'frozen', False):
             BASE_DIR = os.path.dirname(sys.executable)
@@ -20,6 +29,45 @@ class ControlWindow(ctk.CTk):
         else:
             BASE_DIR = os.path.dirname(os.path.abspath(__file__))
             cmd = ["python", f"{BASE_DIR}/service.py", "install"]
+
+        print(cmd)
+        
+        Popen(cmd)
+
+    def remove_service(self):
+        if getattr(sys, 'frozen', False):
+            BASE_DIR = os.path.dirname(sys.executable)
+            full_path = f"{BASE_DIR}\\service.exe".replace("\\", "/")
+            cmd = [ f"{full_path}", "remove"]
+        else:
+            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+            cmd = ["python", f"{BASE_DIR}/service.py", "remove"]
+
+        print(cmd)
+        
+        Popen(cmd)
+
+    def start_service(self):
+        if getattr(sys, 'frozen', False):
+            BASE_DIR = os.path.dirname(sys.executable)
+            full_path = f"{BASE_DIR}\\service.exe".replace("\\", "/")
+            cmd = [ f"{full_path}", "start"]
+        else:
+            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+            cmd = ["python", f"{BASE_DIR}/service.py", "start"]
+
+        print(cmd)
+        
+        Popen(cmd)
+
+    def stop_service(self):
+        if getattr(sys, 'frozen', False):
+            BASE_DIR = os.path.dirname(sys.executable)
+            full_path = f"{BASE_DIR}\\service.exe".replace("\\", "/")
+            cmd = [ f"{full_path}", "stop"]
+        else:
+            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+            cmd = ["python", f"{BASE_DIR}/service.py", "stop"]
 
         print(cmd)
         
